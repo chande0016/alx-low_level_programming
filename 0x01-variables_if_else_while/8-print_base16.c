e8007a2d0f6de417e46a6c4c758bd9ff4a97e10a
chande0016 <chande0016@gmail.com>
2022-04-10 13:10:48 -0700

    commit for task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return; Always 0 (Success/Correct)
+ */
+
+int main(void)
+{
+char b16;
+
+for (b16 = 48 ; b16 <= 57 ; b16++)
+{
+putchar(b16);
+}
+
+for (b16 = 97 ; b16 <= 102 ; b16++)
+{
+putchar(b16);
+}
+
+putchar('\n');
+
+return (0);
+
+}
