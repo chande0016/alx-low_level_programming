71f61c803ea2d3ae74f094092e6936d6af660606
chande0016 <chande0016@gmail.com>
2022-04-10 07:42:00 -0700

    commit massage for alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,12 @@
  *
  *Return: Always 0 (Success/Correct)
  */
+
 int main(void)
 {
 char alphabet;
 
-for (alphabet == 'a' ; alphabet <= 'z' ; alphabet++)
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 {
 putchar(alphabet);
 }
