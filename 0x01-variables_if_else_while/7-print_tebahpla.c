53eb041e02ebc2e4d958720a3f817b90ce03cda0
chande0016 <chande0016@gmail.com>
2022-04-10 12:52:11 -0700

    sms for tasks 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success/Correct)
+
+ */
+int main(void)
+{
+char alpha;
+
+for (alpha = 122 ; alpha >= 97 ; alpha++)
+{
+putchar(alpha);
+}
+
+putchar('\n');
+
+return (0);
+
+}
