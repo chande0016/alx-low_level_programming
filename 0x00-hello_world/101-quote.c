1fc085aa7ee9c6d2ceef7ed7c1f1a7051e7a66b1
chande0016 <chande0016@gmail.com>
2022-04-11 07:36:07 -0700

    task advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+putchar("and that pieceof art is useful\" - Dora korpar, 2015-10-19");
+}
+
+putchar('\n');
+
+return (1);
+
+}
