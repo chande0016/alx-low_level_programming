e9567ffdf174d51985ce392cf44a49f9c2b82c25
chande0016 <chande0016@gmail.com>
2022-04-20 12:41:31 -0700

    tasks 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,12 @@ void puts_half(char *str)
 int i, last;
 
 i = 0;
-while(str[i] != '\0')
+while (str[i] != '\0')
 {
 i++;
 }
 
-last = (i - 1) / 2;
+last = (i + 1) / 2;
 
 for (i = last ; str[i] ; i++)
 {
