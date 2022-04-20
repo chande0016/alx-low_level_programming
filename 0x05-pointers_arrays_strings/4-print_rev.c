a606c59c697629639ea88e9040a451cd974f8c5f
chande0016 <chande0016@gmail.com>
2022-04-20 08:08:39 -0700

    my commit sms for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function that print string rev
+ * @s: A pointer
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (i = i - 1 ; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+_putchar ('\n');
+}
