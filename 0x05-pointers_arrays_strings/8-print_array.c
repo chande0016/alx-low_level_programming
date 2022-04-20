c0effa1fb2497eee8e4dc31ad8dfc168583cd814
chande0016 <chande0016@gmail.com>
2022-04-20 12:54:33 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * print_array - Fuction that print an array
+ *@a: A pointer
+ *@n: return value n
+ *Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int j;
+
+j = 0;
+while (i < n)
+{
+printf("%d", a[j]);
+
+if (i < n - 1)
+{
+printf(", ");
+}
+
+j++;
+
+}
+
+printf("\n");
+}
