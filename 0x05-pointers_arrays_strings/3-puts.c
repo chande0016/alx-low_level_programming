4518d17af434648056e265d2e33a942fad539206
chande0016 <chande0016@gmail.com>
2022-04-20 07:38:05 -0700

    My commmit sms for tasks 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - function print string
+ * @str: a pointer
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+char *c;
+int j;
+
+c = str;
+
+for (j = 0; c[j}; j++)
+{
+_putchar (c[j]);
+}
+_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c
+ * @c: The character to print
+ *
+ * Return: 1
+ * On error, -1 is return
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
