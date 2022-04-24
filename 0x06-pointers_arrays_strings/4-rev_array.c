1c9d39da8ed7f54a95554e0006f308f368be5f2d
chande0016 <chande0016@gmail.com>
2022-04-23 21:34:52 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,10 +14,10 @@ int i, j, temp;
 for (i = 0; i < n - 1; i++)
 {
 for (j = i + 1; j > 0; j--)
-										{
-										temp = *(a + j);
-										*(a + j) = *(a + (j - 1));
-																				*(a + (j - 1)) = temp;
-										}
+{
+temp = *(a + j);
+*(a + j) = *(a + (j - 1));
+*(a + (j - 1)) = temp;
+}
 }
 }
