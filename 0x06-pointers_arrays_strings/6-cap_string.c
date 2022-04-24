a165f49a3533b36e295a08d1e7566b1374c4f16b
chande0016 <chande0016@gmail.com>
2022-04-23 21:30:14 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,12 @@ if (*(s + count) >= 97 && *(s + count) <= 122)
 count++;
 while (*(s + count) != '\0')
 {
-										for (i = 0; i < 13; i++)
-										{
-										if (*(s + count) == sep_words[i])
-										{
-										if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-										*(s + (count + 1)) = *(s + (count + 1)) - 32;
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == sep_words[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
 break;
 }
 }
