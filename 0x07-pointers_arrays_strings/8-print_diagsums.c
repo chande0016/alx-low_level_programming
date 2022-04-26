6d85a769ff218df368b6dffea78b5efd089c0b92
chande0016 <chande0016@gmail.com>
2022-04-26 13:11:36 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,17 @@
 
 void print_diagsums(int *a, int size)
 {
-int i = 0, total1 = 0, total2 = 0;
+int i, total1 = 0, total2 = 0;
 
+i = 0;
 while (i < size)
 {
 total1 += a[(size + 1) * i];
 total2 += a[(size - 1) * (i + 1)];
+
 i++;
 }
+
 printf("%d, %d\n", total1, total2);
 
 }
