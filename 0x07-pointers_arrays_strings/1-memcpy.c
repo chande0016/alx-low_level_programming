bf6b7579d5d08bff5ba3436cb4b3dd20f3ac14cf
chande0016 <chande0016@gmail.com>
2022-04-26 07:58:53 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory
+ * @dest: dest memory
+ * @src: source mamory
+ * @n: bytes from memory
+ * Return: memory area
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+
+a = 0;
+while (a < n)
+{
+dest[a] = src[a];
+a++;
+}
