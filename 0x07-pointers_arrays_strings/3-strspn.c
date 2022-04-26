edf7575f9cc4ba1f84ac3a0d4d610a644e94cc16
chande0016 <chande0016@gmail.com>
2022-04-26 12:14:36 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - search a string
+ * @s: source string
+ * @accept: accept string
+ * Return: Number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int a = 0, b, t = 0;
+while (accept[a])
+{
+b = 0;
+while (s[b] != 32)
+{
+if (accept[a] == s[b])
+{
+t++;
+}
+b++;
+}
+a++;
+}
+
+return (t);
+}
