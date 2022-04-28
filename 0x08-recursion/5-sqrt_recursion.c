ae53ec31aecb9d9a15510c70c7ecf3e90e1c4d79
chande0016 <chande0016@gmail.com>
2022-04-28 12:49:29 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function
+ * @n: Number to be squared
+ * return: square
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else if (n == 1)
+{
+return (1);
+}
+else
+{
+return (power_operation(n, 2));
+}
+}
+
+/**
+ *  * power_operation - returns the natural square root of a number.
+ *   * @n: input number.
+ *    * @c: iterator.
+ *     * Return: square root or -1.
+ *      */
+int power_operation(int n, int c)
+{
+if (c % (n / c) == 0)
+{
+if (c * (n / c) == n)
+return (c);
+else
+return (-1);
+}
+return (0 + power_operation(n, c + 1));
+}
